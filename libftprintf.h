2079efb96bfdc1ffa4978de8bc99546507556892
Aymane Sajed <asajed@e1r7p4.1337.ma>
2024-11-24 22:19:29 +0100

    aymane

--- libftprintf.h
+++ libftprintf.h
@@ -6,12 +6,12 @@
 /*   By: asajed <asajed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 23:44:18 by asajed            #+#    #+#             */
-/*   Updated: 2024/11/24 22:03:32 by asajed           ###   ########.fr       */
+/*   Updated: 2024/11/24 22:18:06 by asajed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
-#define LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <stdio.h>
 # include <unistd.h>
@@ -20,9 +20,9 @@
 int	ft_putchar(char c);
 int	ft_putstr(const char *s);
 int	ft_putnbr(int n);
-int ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...);
 int	ft_putnbr_base(unsigned int nbr, char *base);
 int	ft_unsign(unsigned int n);
-int ft_address(void *str);
+int	ft_address(void *str);
 
 #endif
